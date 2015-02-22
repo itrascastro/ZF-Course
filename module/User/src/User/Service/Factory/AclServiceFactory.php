db6f0da4393e7deed434a1910d976cda5fe4c6c1
itrascastro <itrascastro@gmail.com>
2015-02-23 00:21:00 +0100

    Removing repeated code from AclServiceFactory

--- module/User/src/User/Service/Factory/AclServiceFactory.php
+++ module/User/src/User/Service/Factory/AclServiceFactory.php
@@ -41,62 +41,94 @@ class AclServiceFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $this->acl = new Acl();
-
-        $config = $serviceLocator->get('config');
-        $this->roles = $config['application']['roles'];
+        $this->acl      = new Acl();
+        $config         = $serviceLocator->get('config');
+        $this->roles    = $config['application']['roles'];
+        $routes         = $config['router']['routes'];
 
         foreach ($this->roles as $role) {
             $this->acl->addRole($role);
         }
 
-        $routes = $config['router']['routes'];
-
         foreach ($routes as $route => $value) {
             $this->parseRoute($route, $value);
         }
 
+        var_dump($this->acl);
         return $this->acl;
     }
 
+    /**
+     * parseRoute
+     *
+     * For each route
+     *      - It has not a parent route
+     *          - It has not child routes
+     *          - It has child routes
+     *              - It can be alone (may_terminate == true)
+     *              - It can't be alone (may_terminate == false)
+     *      - It has a parent route
+     *          - It has not child routes
+     *          - It has child routes
+     *              - It can be alone (may_terminate == true)
+     *              - It can't be alone (may_terminate == false)
+     *
+     * @param string $route
+     * @param array $value
+     * @param string $parent
+     */
     private function parseRoute($route, $value, $parent = null)
     {
         if (!$parent) {
             if (empty($value['child_routes'])) {
-                $this->acl->addResource($route);
-                $routeRoles = !empty($value['options']['defaults']['roles']) ? $value['options']['defaults']['roles'] : $this->roles;
-                $this->acl->allow($routeRoles, $route);
+                $this->routeRolesToAcl($route, $value);
             } elseif ($value['may_terminate']) {
-                $this->acl->addResource($route);
-                $routeRoles = !empty($value['options']['defaults']['roles']) ? $value['options']['defaults']['roles'] : $this->roles;
-                $this->acl->allow($routeRoles, $route);
-
-                foreach ($value['child_routes'] as $childRoute => $childValue) {
-                    $this->parseRoute($childRoute, $childValue, $route);
-                }
+                $this->routeRolesToAcl($route, $value);
+                $this->iterateChilds($route, $value);
             } else {
-                foreach ($value['child_routes'] as $childRoute => $childValue) {
-                    $this->parseRoute($childRoute, $childValue, $route);
-                }
+                $this->iterateChilds($route, $value);
             }
         } else {
-            if (empty($value['child_routes'])) {
-                $this->acl->addResource($parent . '/' . $route);
-                $routeRoles = !empty($value['options']['defaults']['roles']) ? $value['options']['defaults']['roles'] : $this->roles;
-                $this->acl->allow($routeRoles, $parent . '/' . $route);
-            } elseif ($value['may_terminate']) {
-                $this->acl->addResource($parent . '/' . $route);
-                $routeRoles = !empty($value['options']['defaults']['roles']) ? $value['options']['defaults']['roles'] : $this->roles;
-                $this->acl->allow($routeRoles, $parent . '/' . $route);
+            $route = $parent . '/' . $route;
 
-                foreach ($value['child_routes'] as $childRoute => $childValue) {
-                    $this->parseRoute($childRoute, $childValue, $parent . '/' . $route);
-                }
+            if (empty($value['child_routes'])) {
+                $this->routeRolesToAcl($route, $value);
+            } elseif ($value['may_terminate']) {
+                $this->routeRolesToAcl($route, $value);
+                $this->iterateChilds($route, $value);
             } else {
-                foreach ($value['child_routes'] as $childRoute => $childValue) {
-                    $this->parseRoute($childRoute, $childValue, $parent . '/' . $route);
-                }
+                $this->iterateChilds($route, $value);
             }
+        }
+    }
+
+    /**
+     * routeRolesToAcl
+     *
+     * Creates an allow rule in Acl for that route and its allowed roles
+     *
+     * @param string $route
+     * @param array $value
+     */
+    private function routeRolesToAcl($route, $value)
+    {
+        $this->acl->addResource($route);
+        $routeRoles = !empty($value['options']['defaults']['roles']) ? $value['options']['defaults']['roles'] : $this->roles;
+        $this->acl->allow($routeRoles, $route);
+    }
+
+    /**
+     * iterateChilds
+     *
+     * Iterates child routes for a given route parsing each child
+     *
+     * @param string $route
+     * @param array $value
+     */
+    private function iterateChilds($route, $value)
+    {
+        foreach ($value['child_routes'] as $childRoute => $childValue) {
+            $this->parseRoute($childRoute, $childValue, $route);
         }
     }
 }
